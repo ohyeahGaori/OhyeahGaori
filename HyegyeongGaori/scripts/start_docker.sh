f92c40159b2459ec9e5ea88ee1220332a378603e
BakHyegyeong <calla3494qhk@naver.com>
2024-09-22 22:04:56 +0900

    fix : start_docker.sh 내용 수정

--- HyegyeongGaori/scripts/start_docker.sh
+++ HyegyeongGaori/scripts/start_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo docker pull bakhyegyeong/gaori_backend
 sudo docker ps -q --filter name=spring | grep -q . && docker rm -f $(docker ps -aq --filter name=spring)
-sudo docker run -d --name spring -p 9090:9090 bakhyegyeong/gaori_backend
+sudo docker run -d -e SPRING_PROFILES_ACTIVE=local --name spring -p 9090:9090 bakhyegyeong/gaori_backend 
